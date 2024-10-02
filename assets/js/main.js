979b18cff32d7d33f6fb34e3d5f97c2cc0d6b6ae
Tamim <tamimhossain031@gmail.com>
2024-10-02 18:58:29 +0600

    add update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,52 @@
 const items = document.querySelectorAll('.item');
-const image = document.querySelector('.image');
+const image = document.querySelector('.image-holder');
 const outer = document.querySelector('.outer');
+const bg_image = document.querySelector('.bg-image img');
 const hoverBg = document.querySelector('.hover-bg');
+const box = document.querySelector('.box');
+const images = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg']
 items.forEach((item,index) =>{
     
-    item.addEventListener('mouseover',(e)=>{
-        e.preventDefault();        
-        item.classList.add('active');
-        const data = item.getAttribute('data');
-        hoverBg.style.top = `${index * 80}px`;
-        image.style.backgroundImage = `url(${data})`;
-        image.style.transition = 'all 1s ease';
-        outer.style.backgroundImage = `url(${data})`
-    })
-    item.addEventListener('mouseout',(e)=>{
-        e.preventDefault();
+    item.addEventListener('mouseover',function(e){               
+        document.querySelector('.active').classList.remove('active');                    
+       
+        const data = `./assets/img/${images[index]}`;
+        hoverBg.style.top = `${index * 80}px`;     
+      
+        if(!item.classList.contains('active')){ 
+            item.classList.add('active');
+            image.style.opacity='0.5';
+            bg_image.style.opacity='0.5';
+            const time = setTimeout(()=>{            
+                image.src = data ;
+                bg_image.src = data ;
+                image.style.opacity='1';
+                bg_image.style.opacity='1';
+            },200);
+        }else{
+            image.src =`${data}` ;
+            bg_image.src =`${data}` ;
+          
+        }
+      
+      
+            
+           
+       
+       
         
-        item.classList.remove('active');
-    })
+    },false)
+   
+  
 }
    
 )
+
+const fade = () =>{
+    outer.style.opacity = '1';
+    image.style.opacity = '1';
+   
+}
+window.addEventListener('load',()=>{
+    fade()
+})
