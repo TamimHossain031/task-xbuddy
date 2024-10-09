7bd5528ef902180667d6958fceb320c4a6dbb643
Tamim <tamimhossain031@gmail.com>
2024-10-09 23:40:20 +0600

    add correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,39 +4,34 @@ const outer = document.querySelector('.outer');
 const bg_image = document.querySelector('.bg-image img');
 const hoverBg = document.querySelector('.hover-bg');
 const box = document.querySelector('.box');
+let time;
+let dataAll;
 const images = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg']
 items.forEach((item,index) =>{
-    
-    item.addEventListener('mouseover',function(e){               
-        document.querySelector('.active').classList.remove('active');                    
+    const newHover = document.createElement('div');
+    newHover.classList.add('hovered');
+    item.appendChild(newHover);
+    newHover.addEventListener('mouseover',function(e){               
+      document.querySelectorAll('.active').forEach(single => single.classList.remove('active'))                 
        
         const data = `./assets/img/${images[index]}`;
         hoverBg.style.top = `${index * 80}px`;     
       
         if(!item.classList.contains('active')){ 
+            clearTimeout(time);
             item.classList.add('active');
-            image.style.opacity='0.5';
-            bg_image.style.opacity='0.5';
-            const time = setTimeout(()=>{            
+            image.classList.add('active');
+            time = setTimeout(()=>{            
                 image.src = data ;
                 bg_image.src = data ;
-                image.style.opacity='1';
-                bg_image.style.opacity='1';
+              
             },200);
         }else{
-            image.src =`${data}` ;
-            bg_image.src =`${data}` ;
-          
-        }
-      
-      
-            
            
-       
-       
+         
+        }      
         
-    },false)
-   
+    },true) 
   
 }
    
